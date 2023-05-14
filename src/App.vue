598a4f6ff22141aeb810bd18e604dfccf8c0b65a
ITXYGod <1938269430@qq.com>
2023-05-14 16:42:51 +0800

    静态页面准备

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
-
+     <div class="todoapp">
+         <TodoHeader></TodoHeader>
+         <TodoMain></TodoMain>
+         <TodoFooter></TodoFooter>
+     </div>
   </div>
 </template>
 
 <script>
+import './styles/base.css';
+import './styles/index.css';
+// 引入组件
+import TodoHeader from './components/TodoHeader.vue';
+import TodoMain from './components/TodoMain.vue';
+import TodoFooter from './components/TodoFooter.vue';
 export default {
-
+  components: {
+    TodoHeader,
+    TodoMain,
+    TodoFooter
+  }
 }
 </script>
 
